d7c2ca2c7481ce2b763e84e04dc841c173852f5c
niicool <125748273+niicool@users.noreply.github.com>
2023-02-18 16:04:19 +0100

    Add files via upload

---
+++ style.css
@@ -0,0 +1,64 @@
+ <html lang="es">
+<head>
+ <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title> Formulario </title>
+<h1 style="color:#6FCCD1;text-align:center;"> Create Account </h1>
+
+</head>
+
+<body style="background-color:#254859;">
+<script>
+function comprobarClave() {
+      clave1 = document.f1.clave1.value
+      clave2 = document.f1.clave2.value
+
+      if (clave1 == clave2) {
+        alert("Las dos claves son iguales...\nRealizaríamos las acciones del caso positivo")
+      } else {
+        alert("Las dos claves son distintas...\nRealizaríamos las acciones del caso negativo")
+      }
+    }
+<input:invalid {
+  border: 2px dashed red;}
+
+input:valid {
+  border: 2px solid green;}/>
+
+  </script>
+
+<center><table width="900"><table style="background-color:#9D6FD1;">
+<tr><td>
+
+<h2>Nombre: </h2>
+<input type="text" name="nombre" value="" required/>
+<br />
+
+<h2>Email: </h2>
+<input type="email" name="ecorreo" pattern="[A-Za-z0-9]+ \@"/required/>
+<function isValidEmail(mail) {return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(mail); }/>
+ <br /><br />
+
+<form action="" name="f1">
+<h2> Clave: </h2>
+<input type="password" name="clave1" pattern="[A-Za-z0-9]{5,8}"  required/>
+ <br /><br />
+
+<h2> Confirmar su clave: </h2>
+<input type="password"  name="clave2" pattern="[A-Za-z0-9]{5,8}"required /> 
+<br /><br />
+
+
+<input type="submit" class="button" name= "enviar" value="Enviar"  />
+<br />
+
+</center></table></tr></td>
+</form>
+
+
+
+</center></table></tr></td>
+</body>
+</html>
